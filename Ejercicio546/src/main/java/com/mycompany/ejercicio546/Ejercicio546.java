cabbeb891cbbcc91cfbccbccc5ade1f88031c330
Mariana088 <margarciahe@unal.edu.co>
2025-12-07 18:44:44 -0500

    Add files via upload

---
+++ Ejercicio546/src/main/java/com/mycompany/ejercicio546/Ejercicio546.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio546;
+
+/**
+ *
+ * @author lane
+ */
+public class Ejercicio546 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
